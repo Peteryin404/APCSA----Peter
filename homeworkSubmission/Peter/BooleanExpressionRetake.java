86b5ff3621e0228fb57ee232dbf432338f649231
Peteryin404 <109002254+Peteryin404@users.noreply.github.com>
2025-10-16 15:46:05 -0700

    Update BooleanExpressionRetake.java

--- homeworkSubmission/Peter/BooleanExpressionRetake.java
+++ homeworkSubmission/Peter/BooleanExpressionRetake.java
@@ -1,1 +1,25 @@
+public cigarParty(int cigars, boolean isWeekend)
+{
+  return (cigars >= 40 && cigars <= 60) || (isWeekend && cigars >= 40);
+}
 
+
+public int dateFashion(int you, int date)
+{
+  return (you >= 8 || date >= 8) || (you <=2 || date <= 2);
+}
+
+
+public boolean squirrelPlay(int temp, boolean isSummer)
+{
+  return (temp >= 60 && temp <= 90) || (isSummer && temp <= 100);
+}
+
+
+public boolean makeBricks(int small, int big, int goal)
+{
+
+}
+
+
+public 
