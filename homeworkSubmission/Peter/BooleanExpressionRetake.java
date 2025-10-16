df7b01cfc038853ee3346f108892c5f23c128e6e
Peteryin404 <109002254+Peteryin404@users.noreply.github.com>
2025-10-16 15:47:52 -0700

    Update BooleanExpressionRetake.java

--- homeworkSubmission/Peter/BooleanExpressionRetake.java
+++ homeworkSubmission/Peter/BooleanExpressionRetake.java
@@ -18,7 +18,7 @@ public boolean squirrelPlay(int temp, boolean isSummer)
 
 public boolean makeBricks(int small, int big, int goal)
 {
-
+  return ((goal <= small + big * 5) && (goal % 5 <= small));
 }
 
 
