719575e1e8de891e8fa7bdd545eddd696b7be7f3
Peteryin404 <109002254+Peteryin404@users.noreply.github.com>
2025-10-27 17:00:41 -0700

    Update debugPuzzles.java

--- Homework/debugPuzzles.java
+++ Homework/debugPuzzles.java
@@ -1,7 +1,9 @@
 // debug the following code, use your brain and paper. If you couldn't find the errors, use your IDE.
 
-public class SimplePuzzle {
-    public static void main(String[] args) {
+public class SimplePuzzle 
+{
+    public static void main(String[] args) 
+    {
         String keyword = "secret";
         String guess = "secret";
 
@@ -13,35 +15,47 @@ public class SimplePuzzle {
         boolean isDivisible;
         
         // Puzzle 1: Compare the strings
-        if (guess == keyword) {
+        if (guess == keyword) 
+        {
             isCorrectGuess = true;
-        } else {
+        } 
+        else 
+        {
             isCorrectGuess = false;
         }
 
         // Puzzle 2: Find the substring
         int firstIndex = keyword.indexOf("cre");
-        if (firstIndex > 0) {
+        if (firstIndex > 0)
+        {
             hasSubstring = true;
-        } else {
+        } 
+        else 
+        {
             hasSubstring = false;
         }
 
         // Puzzle 3: Check divisibility
-        if (number / check == 0) {
+        if (number / check == 0) 
+        {
             isDivisible = true;
-        } else {
+        } 
+        else 
+        {
             isDivisible = false;
         }
         
         // The final result
-        if (isCorrectGuess && hasSubstring && isDivisible) {
+        if (isCorrectGuess && hasSubstring && isDivisible) 
+        {
             System.out.println("The puzzle is solved!");
-        } else {
+        } 
+        else 
+        {
             System.out.println("Something is wrong with your logic.");
             System.out.println("isCorrectGuess: " + isCorrectGuess);
             System.out.println("hasSubstring: " + hasSubstring);
             System.out.println("isDivisible: " + isDivisible);
-        
-    }
+        }
+    }   
 }
