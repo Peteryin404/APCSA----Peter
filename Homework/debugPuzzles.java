c15f686d391fee3b515059af33d989ead555583b
Peteryin404 <109002254+Peteryin404@users.noreply.github.com>
2025-10-28 08:24:00 -0700

    Update debugPuzzles.java

--- Homework/debugPuzzles.java
+++ Homework/debugPuzzles.java
@@ -1,9 +1,7 @@
 // debug the following code, use your brain and paper. If you couldn't find the errors, use your IDE.
 
-public class SimplePuzzle 
-{
-    public static void main(String[] args) 
-    {
+public class SimplePuzzle {
+    public static void main(String[] args) {
         String keyword = "secret";
         String guess = "secret";
 
@@ -13,49 +11,25 @@ public class SimplePuzzle
         boolean isCorrectGuess;
         boolean hasSubstring;
         boolean isDivisible;
-        
-        // Puzzle 1: Compare the strings
-        if (guess == keyword) 
-        {
-            isCorrectGuess = true;
-        } 
-        else 
-        {
-            isCorrectGuess = false;
-        }
 
-        // Puzzle 2: Find the substring
+        // Puzzle 1: Compare the strings (use equals)
+        isCorrectGuess = guess.equals(keyword);
+
+        // Puzzle 2: Find the substring (indexOf can be 0)
         int firstIndex = keyword.indexOf("cre");
-        if (firstIndex > 0)
-        {
-            hasSubstring = true;
-        } 
-        else 
-        {
-            hasSubstring = false;
-        }
+        hasSubstring = (firstIndex >= 0);
 
-        // Puzzle 3: Check divisibility
-        if (number / check == 0) 
-        {
-            isDivisible = true;
-        } 
-        else 
-        {
-            isDivisible = false;
-        }
-        
+        // Puzzle 3: Check divisibility (use modulus %)
+        isDivisible = (number % check == 0);
+
         // The final result
-        if (isCorrectGuess && hasSubstring && isDivisible) 
-        {
+        if (isCorrectGuess && hasSubstring && isDivisible) {
             System.out.println("The puzzle is solved!");
-        } 
-        else 
-        {
+        } else {
             System.out.println("Something is wrong with your logic.");
             System.out.println("isCorrectGuess: " + isCorrectGuess);
             System.out.println("hasSubstring: " + hasSubstring);
             System.out.println("isDivisible: " + isDivisible);
         }
-    }   
+    }
 }
